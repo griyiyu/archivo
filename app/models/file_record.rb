111236ca87876aaaedfc0eab09062baa5f1e0c1b
grigit <ariasgri@gmail.com>
2014-07-08 19:01:28 -0300

    separacion para mejor lectura

--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -2,6 +2,7 @@ class FileRecord < ActiveRecord::Base
   has_many :steps, dependent: :destroy
   
   after_save :save_step
+  
   def save_step
     office = Office.where(name: 'Mesa de entrada').first
     steps.create({ :office => office })
