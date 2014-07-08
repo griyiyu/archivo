90fc4edde4795dd77b8222f01b53396e6cfe790e
grigit <ariasgri@gmail.com>
2014-07-08 19:32:22 -0300

    cambios para que muestre el step por mas que no tenga una persona asociada

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,23 @@ class Step < ActiveRecord::Base
   belongs_to :file_record
   belongs_to :person
   belongs_to :office
+  
+  # Crea una nueva instancia de step que tiene la oficina "mesa de entrada"
+  def start
+    office = Office.where(name: 'Mesa de entrada').first
+    create({ :office => office })
+  end
+  
+  
+  # Crea una nueva instancia de Step ue tiene la oficina "Archivo" y si no existe la oficina la crea
+  def archivate
+    officeName = 'Archivo'
+    office = Office.where(name: officeName).first
+    if (office == nil)
+      Office.create({ :name => officeName })
+      office = Office.where(name: officeName).first
+    end
+    create({ :office => office })
+  end
+  
 end
